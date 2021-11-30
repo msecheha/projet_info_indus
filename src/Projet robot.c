a5f36575ea59056e42a3c7d062a74098c6137f0e
msecheha <maxime.sechehaye@ens-paris-saclay.fr>
2021-11-30 12:42:33 +0100

    Generation impulsion initiale

--- src/Projet robot.c
+++ src/Projet robot.c
@@ -22,6 +22,13 @@
 #include "dac.h"
 #include "lib_ENS_II1_lcd.h"
 
+#define TRIG LPC_GPIO_PORT->B0[17]
+#define ECHO LPC_GPIO_PORT->B0[16]
+#define M1SR LPC_GPIO_PORT->B0[14]
+#define M1PWM LPC_GPIO_PORT->B0[15]
+#define M2SR LPC_GPIO_PORT->B0[18]
+#define M2PWM LPC_GPIO_PORT->B0[20]
+
 int main(void) {
 
 	//Activation du périphérique d'entrées/sorties TOR
@@ -33,7 +40,23 @@ int main(void) {
 	// Configuration de l'horloge a sa frequence maximale
 	LPC_PWRD_API->set_fro_frequency(30000);
 
+	LPC_CTIMER0->PR = 14;	// TC s'incremente toutes les microsecondes
+
 	return 0;
 
+}
+
+void impulsion_capteur()
+	// Fonction qui genere l'impulsion de 10 microsecondes
+	// necessaire pour lancer la salve d'ultrasons
+	{
+	LPC_CTIMER0->TCR = (1 << CEN);
+	while (LPC_CTIMER0->TC < 10) {
+		LPC_GPIO_PORT->PIN0 = (1 << 17);
+		// On genere une impulsion de 10 microsecondes
+	}
+	LPC_GPIO_PORT->NOT0 = (1 << 17);
+	LPC_CTIMER0->TCR = (0 << CEN);
+	// On pense a desactiver le compteur apres
 }
 
